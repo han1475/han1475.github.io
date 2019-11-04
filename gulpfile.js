0e5df1f8da418158bb83ae9ba48aa12d7b89c745
han1475 <me@han1475.com>
2019-11-05 00:07:41 +0800

    gulp task change parallel to series

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,6 @@ gulp.task('minify-images', function() {
 });
 
 
-gulp.task('default', gulp.parallel('minify-html','minify-css','minify-js','minify-images'),function(){
+gulp.task('default', gulp.series('minify-html','minify-css','minify-js','minify-images'),function(){
     
 });
